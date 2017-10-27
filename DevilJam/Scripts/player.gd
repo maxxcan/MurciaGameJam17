6394e67131f2d561a9a34278e8281eb394fb41ba
Antonio <toniocga@gmail.com>
2017-10-27 22:02:38 +0200

    Ajustado impulso del player

--- DevilJam/Scripts/player.gd
+++ DevilJam/Scripts/player.gd
@@ -1,17 +1,13 @@
 extends RigidBody2D
 
-export var fuerza_toque = -100
+export var fuerza_toque = -500
 
 func _ready():
 	set_process_input(true)
-	set_fixed_process(true)
 	
 func _input(event):
 	if event.is_action_pressed("toque"):
 		apply_impulse(Vector2(0,0), Vector2(0,fuerza_toque))
 		print (get_linear_velocity().y)
 		
-func _fixed_process(delta):
-	print(get_linear_velocity())
-	if get_linear_velocity().y < -400:
-		set_linear_velocity(Vector2(0,20))
+
